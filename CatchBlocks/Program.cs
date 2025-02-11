c74566e6a0c371062372477aa08f000e11449cbc
mm1009057 <mm1009057@otc.edu>
2025-02-11 10:05:48 -0600

    initial commit

---
+++ CatchBlocks/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string input = "49"; // or use "49" in a notebook
+        try
+        {
+            int age = int.Parse(input);
+            Console.WriteLine($"You are {age} years old.");
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine("The age you entered is not a valid number format");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{ex.GetType()} says {ex.Message}");
+        }
+    }
+}
